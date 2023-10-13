73bcb9a7d653345d6c4e94e05a58a5f81033d677
mushuye <hones_tan@outlook.com>
2023-10-14 01:56:36 +0800

    Site updated: 2023-10-14 01:56:09

--- js/ip_content.js
+++ js/ip_content.js
@@ -1,32 +1,5 @@
 //获取当前IP地址和浏览器标识
-function getBrowserInfo() {
-    var agent = navigator.userAgent.toLowerCase();
 
-    var regStr_ie = /msie [\d.]+;/gi;
-    var regStr_ff = /firefox\/[\d.]+/gi
-    var regStr_chrome = /chrome\/[\d.]+/gi;
-    var regStr_saf = /safari\/[\d.]+/gi;
-
-    //IE
-    if (agent.indexOf("msie") > 0) {
-        return agent.match(regStr_ie);
-    }
-
-    //firefox
-    if (agent.indexOf("firefox") > 0) {
-        return agent.match(regStr_ff);
-    }
-
-    //Chrome
-    if (agent.indexOf("chrome") > 0) {
-        return agent.match(regStr_chrome);
-    }
-
-    //Safari
-    if (agent.indexOf("safari") > 0 && agent.indexOf("chrome") < 0) {
-        return agent.match(regStr_saf);
-    }
-}
 
 var ip_content = document.querySelector(".ip_content");
 var apiURL = 'https://ip.useragentinfo.com/json?';
@@ -35,7 +8,7 @@ fetch(apiURL)
   .then(response => response.json())
   .then(returnCitySN => {
     if (ip_content != null) {
-      ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.province + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.ip + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
+      ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.province + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.ip + "</span><br>网络版本：<span class='p blue'>" + returnCitySN.city + returnCitySN.isp + returnCitySN.net '</span>';
     }
   })
   .catch(error => console.error('获取数据时出错: ' + error));
