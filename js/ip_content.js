18a2333447ec188e9ea9c0e0a74460a72effccae
mushuye <hones_tan@outlook.com>
2023-10-13 17:26:21 +0800

    Site updated: 2023-10-13 17:26:12

--- js/ip_content.js
+++ js/ip_content.js
@@ -27,9 +27,9 @@ function getBrowserInfo() {
         return agent.match(regStr_saf);
     }
 }
-var returnCitySN= 'http://ip-api.com/json'
-var ip_content = document.querySelector(".ip_content");
 
+var ip_content = document.querySelector(".ip_content");
+var returnCitySN = 'http://ip-api.com/json'
 if (ip_content != null && typeof (returnCitySN) != undefined) {
     ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.regionName + returnCitySN["city"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["query"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
 }
