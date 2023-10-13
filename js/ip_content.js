e6f8d306152fbc5f5f40ab637f44d4ce0bb06870
mushuye <hones_tan@outlook.com>
2023-10-13 18:00:32 +0800

    Site updated: 2023-10-13 18:00:24

--- js/ip_content.js
+++ js/ip_content.js
@@ -29,13 +29,13 @@ function getBrowserInfo() {
 }
 
 var ip_content = document.querySelector(".ip_content");
-var apiURL = 'http://ip-api.com/json';
+var apiURL = 'https://ipapi.co/json';
 
 fetch(apiURL)
   .then(response => response.json())
   .then(returnCitySN => {
     if (ip_content != null) {
-      ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.regionName + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.query + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
+      ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.region + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.ip + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
     }
   })
   .catch(error => console.error('获取数据时出错: ' + error));
