8c0dbd19b35ab44e5ebcbc80e45d68b2147cd7de
mushuye <hones_tan@outlook.com>
2023-10-13 15:51:19 +0800

    Site updated: 2023-10-13 15:51:10

--- js/ip_content.js
+++ js/ip_content.js
@@ -1,35 +1,12 @@
-function getBrowserInfo() {
-    var agent = navigator.userAgent.toLowerCase();
-
-    var regStr_ie = /msie [\d.]+;/gi;
-    var regStr_ff = /firefox\/[\d.]+/gi
-    var regStr_chrome = /chrome\/[\d.]+/gi;
-    var regStr_saf = /safari\/[\d.]+/gi;
-
-    //IE
-    if (agent.indexOf("msie") > 0) {
-        return agent.match(regStr_ie);
-    }
-
-    //firefox
-    if (agent.indexOf("firefox") > 0) {
-        return agent.match(regStr_ff);
-    }
-
-    //Chrome
-    if (agent.indexOf("chrome") > 0) {
-        return agent.match(regStr_chrome);
-    }
-
-    //Safari
-    if (agent.indexOf("safari") > 0 && agent.indexOf("chrome") < 0) {
-        return agent.match(regStr_saf);
-    }
-}
-
 var ip_content = document.querySelector(".ip_content");
 
-if (ip_content != null ) {
-    ip_content.innerHTML = '欢迎来自 <span class="p red">' + regionName + city + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["query"] + "</span><br>当下即最好：<span class='p blue'>" + getBrowserInfo() + '</span>';
-}
-
+fetch('https://ipapi.co/json/')
+    .then(response => response.json())
+    .then(data => {
+        ip_content.innerHTML = '欢迎来自 <span class="p red">' + data.city + '</span> 的小伙伴<br>' +
+            '访问IP为：<span class="p cyan">' + data.ip + '</span><br>' +
+            '当下即最好：<span class="p blue">' + getBrowserInfo() + '</span>';
+    })
+    .catch(error => {
+        console.error('无法获取IP地址:', error);
+    });
