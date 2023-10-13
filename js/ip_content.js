425923cd58733b44a2544f7fd0f8c189d9efdc14
mushuye <hones_tan@outlook.com>
2023-10-13 11:18:06 +0800

    Site updated: 2023-10-13 11:17:58

--- js/ip_content.js
+++ js/ip_content.js
@@ -30,6 +30,6 @@ function getBrowserInfo() {
 var ip_content = document.querySelector(".ip_content");
 
 if (ip_content != null && typeof (returnCitySN) != undefined) {
-    ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN["cname"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["cip"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
+    ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN["cname"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["https://api.ipify.org?format=json"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
 }
 
