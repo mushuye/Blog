810d6032fac0a4943e6cb58fe947bcfe5691fd67
mushuye <hones_tan@outlook.com>
2023-10-14 02:04:26 +0800

    Site updated: 2023-10-14 02:04:17

--- js/ip_content.js
+++ js/ip_content.js
@@ -8,7 +8,7 @@ fetch(apiURL)
   .then(response => response.json())
   .then(returnCitySN => {
     if (ip_content != null) {
-      ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.province + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.ip + "</span><br>网络版本：<span class='p blue'>" + returnCitySN.city + returnCitySN.isp + returnCitySN.net '</span>';
+      ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.province + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.ip + "</span><br>网络版本：<span class='p blue'>" + returnCitySN.city + returnCitySN.isp + returnCitySN.net + '</span>';
     }
   })
   .catch(error => console.error('获取数据时出错: ' + error));
