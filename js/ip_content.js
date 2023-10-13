4b4c802d5ca45f2469a423e372dda37b6f67f3cd
mushuye <hones_tan@outlook.com>
2023-10-13 16:44:48 +0800

    Site updated: 2023-10-13 16:44:39

--- js/ip_content.js
+++ js/ip_content.js
@@ -1,12 +1,36 @@
+//获取当前IP地址和浏览器标识
+function getBrowserInfo() {
+    var agent = navigator.userAgent.toLowerCase();
+
+    var regStr_ie = /msie [\d.]+;/gi;
+    var regStr_ff = /firefox\/[\d.]+/gi
+    var regStr_chrome = /chrome\/[\d.]+/gi;
+    var regStr_saf = /safari\/[\d.]+/gi;
+
+    //IE
+    if (agent.indexOf("msie") > 0) {
+        return agent.match(regStr_ie);
+    }
+
+    //firefox
+    if (agent.indexOf("firefox") > 0) {
+        return agent.match(regStr_ff);
+    }
+
+    //Chrome
+    if (agent.indexOf("chrome") > 0) {
+        return agent.match(regStr_chrome);
+    }
+
+    //Safari
+    if (agent.indexOf("safari") > 0 && agent.indexOf("chrome") < 0) {
+        return agent.match(regStr_saf);
+    }
+}
+var returnCitySN= http://ip-api.com/json
 var ip_content = document.querySelector(".ip_content");
 
-fetch('https://ipapi.co/json/')
-    .then(response => response.json())
-    .then(data => {
-        ip_content.innerHTML = '欢迎来自 <span class="p red">' + data.city + '</span> 的小伙伴<br>' +
-            '访问IP为：<span class="p cyan">' + data.ip + '</span><br>' +
-            '当下即最好：<span class="p blue">' + getBrowserInfo() + '</span>';
-    })
-    .catch(error => {
-        console.error('无法获取IP地址:', error);
-    });
+if (ip_content != null && typeof (returnCitySN) != undefined) {
+    ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN["regionName"] + returnCitySN["city"] + "</span> 的小伙伴<br>" + "访问IP为： <span class='p cyan'>" + returnCitySN["query"] + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
+}
+
