f67b3b0d2bc812c8848bf9c8c6dbf551557a795a
mushuye <hones_tan@outlook.com>
2023-10-13 19:06:26 +0800

    Site updated: 2023-10-13 19:06:17

--- js/ip_content.js
+++ js/ip_content.js
@@ -31,11 +31,16 @@ function getBrowserInfo() {
 var ip_content = document.querySelector(".ip_content");
 var apiURL = 'https://ipapi.co/json';
 
+fetch('https://whois.pconline.com.cn/ip.jsp') // 请替换成实际的URL
+  .then(response => response.json())
+  .then(iaddressData => {
+    // 获取 adc.com 中的IP量
+    
 fetch(apiURL)
   .then(response => response.json())
   .then(returnCitySN => {
     if (ip_content != null) {
-      ip_content.innerHTML = '欢迎来自 <span class="p red">' + returnCitySN.region + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.ip + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
+      ip_content.innerHTML = '欢迎来自 <span class="p red">' + iaddressData + returnCitySN.city + "</span> 的小伙伴<br>访问IP为： <span class='p cyan'>" + returnCitySN.ip + "</span><br>浏览器版本：<span class='p blue'>" + getBrowserInfo() + '</span>';
     }
   })
   .catch(error => console.error('获取数据时出错: ' + error));
